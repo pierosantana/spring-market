25da79b067aa5d8cdfd7bccf667cb3ceb962e77a
pierosantana <piero.santanalarosa@gmail.com>
2026-02-18 14:40:28 +0100

    feat: implementar PurchaseItem

---
+++ src/main/java/com/market/domain/PurchaseItem.java
@@ -0,0 +1,50 @@
+package com.market.domain;
+
+public class PurchaseItem {
+    private Integer productId;
+    private Integer quantity;
+    private Double total;
+    private Boolean active;
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Boolean isActive() {
+        return active;
+    }
+
+    @Override
+    public String toString() {
+        return "PurchaseItem{" +
+                "productId=" + productId +
+                ", quantity=" + quantity +
+                ", total=" + total +
+                ", active=" + active +
+                '}';
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
